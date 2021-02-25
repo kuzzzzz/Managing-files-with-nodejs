c448fc24eccc5cfcf93f2c2276a609025e1d4ff8
Atumah Daniel <atumahdaniel@gmail.com>
2021-02-26 00:15:10 +0100

    This is really cool knowing you can create a file like

---
+++ create.index.js
@@ -0,0 +1,22 @@
+const { closeSync, openSync, readdirSync, writeSync } = require("fs");
+const camelCase = require("camelcase");
+
+const indexFd = openSync(
+  "./index.js",
+  "w"
+); /** if we don't set a flag the file will open in read 
+                                                mode and throw an exception when we try to write to it
+                                                */
+
+const files = readdirSync("./read");
+files.map((f) => {
+  const name = f.replace("js", "");
+  console.log(`Adding a file: ${f}`);
+
+  writeSync(
+    indexFd,
+    `module.exports.${camelCase(name)} = require('./read/${name}').read;\n`
+  );
+});
+
+closeSync(indexFd); // remember to close your files everytime
--- synchronous.read.js
+++
@@ -1,9 +0,0 @@
-const { convertCsv } = require("./util/csv.parse.js");
-const { readFileSync } = require("fs");
-
-try {
-  const data = readFileSync("./data/pulitzer-circulation-data.csv", "utf-8");
-  console.table(convertCsv(data));
-} catch (error) {
-  console.log(`There was an error reading this file ${error}`);
-}
