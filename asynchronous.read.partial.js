9717ba1c960c64996db796e50eed17359618dbaa
Atumah Daniel <atumahdaniel@gmail.com>
2021-02-25 01:25:41 +0100

    reading chunks of a file per time using for loop

--- asynchronous.read.partial.js
+++ asynchronous.read.partial.js
@@ -2,8 +2,8 @@ const { convertCsv } = require("./util/csv.parse.js");
 const { open, read } = require("fs");
 
 open("./data/pulitzer-circulation-data.csv", (err, fd) => {
-  const burfer = Buffer.alloc(200);
-  read(fd, burfer, 0, burfer.length, 0, (err, count, buff) => {
+  const buffer = Buffer.alloc(200);
+  read(fd, buffer, 0, buffer.length, 0, (err, count, buff) => {
     console.table(convertCsv(buff.toString()));
   });
 });
