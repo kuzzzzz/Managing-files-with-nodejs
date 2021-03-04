71ae7de0580662298253e78af99dfc43d6d4d634
Atumah Daniel <atumahdaniel@gmail.com>
2021-03-04 05:32:26 +0100

    final module on how to read large files in node

--- write.stream.js
+++ write.stream.js
@@ -19,3 +19,18 @@ stream.on("data", (data) => {
 });
 
 // This code copied data from one file and writes it to another file 95 bytes at a time
+
+/**
+ * Back Pressure
+ * A Backup of data, caused bt streams being unable to
+ * process data before the next batch arrive
+ * 
+ * This back pressure is caused when the output
+ * stream is writing out data slower than it's 
+ * reading data 
+ * 
+ * by replacing the write stream with pipe 
+ * you can write out a file much faster
+ * that is with stream.pipe(writer) 
+ * 
+ */
